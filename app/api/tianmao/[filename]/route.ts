3c8d52ba6da5e73bd440afbc2bcc611cd96dd9c2
yellowwei <1119765689hw@gmail.com>
2026-06-17 14:20:20 +0800

    upload tianmao verification file

---
+++ app/api/tianmao/[filename]/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { filename: string } },
+) {
+  if (params.filename === "2264ae042fd6942ef7b10f92e52b1ebd.txt") {
+    return new NextResponse(
+      "Jfc4Z4Ur15JwUBuvUQD5wg7Nu8+l+HscqYlfofbyJdY4rZ9rR4aoEkVOdVnOVy/A",
+      {
+        headers: {
+          "Content-Type": "text/plain",
+          "Cache-Control": "no-cache",
+        },
+      },
+    );
+  }
+  return new NextResponse("Not Found", { status: 404 });
+}
